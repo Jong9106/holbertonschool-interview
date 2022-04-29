c3d6909de75f604dc78bf2260adfb6013855db5e
Jong9106 <juank_hr@hotmail.com>
2022-04-28 20:51:59 -0500

    ADD | New file

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Function to unlocked box of lists
+"""
+
+def canUnlockAll(boxes):
+    box_unloked = [0]
+
+    for box_index, in_box in enumerate(boxes):
+        if not in_box:
+            continue
+
+        for value in in_box:
+            if value < len(boxes) and value not in box_unloked:
+                if value != box_index:
+                    box_unloked.append(value)
+
+    if len(box_unloked) == len(boxes):
+        return True
+    return False
